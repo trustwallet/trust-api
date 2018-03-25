197804cee93decad3cba9f5df06380a712aeecc1
Mykola <kolya182@gmail.com>
2018-03-25 16:43:02 -0700

    Add route

--- src/routes/ApiRoutes.ts
+++ src/routes/ApiRoutes.ts
@@ -1,11 +1,13 @@
 import  * as express from "express";
 import { DAppsController } from "../controllers/DAppsController";
+import { TokenPriceController } from "../controllers/TokenPriceController";
 const router = express.Router();
 
 const dAppsController = new DAppsController();
+const priceController = new TokenPriceController();
 
-// URLs for tokens
 router.get("/dapps", dAppsController.readAll);
+router.post("/tokenPrices", priceController.getTokenPrices);
 
 export {
     router
