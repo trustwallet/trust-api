b989acd10c33db0f8b9a3cc3e3e300c70fc5f455
Maksim <wdiabloster@gmail.com>
2018-05-29 17:06:17 +0700

    Update apk version

--- src/controllers/AppCheck.ts
+++ src/controllers/AppCheck.ts
@@ -4,9 +4,9 @@ import { sendJSONresponse } from "../common/Utils";
 export class AppCheck {
     public android(req: Request, res: Response) {
         sendJSONresponse(res, 200, {
-            "latestVersion": "1.5.111",
-            "latestVersionCode": 228,
-            "url": "https://s3-us-east-2.amazonaws.com/trustandroidapk/bitrise_e6a70d2c429f5842/latest_build/app-debug-bitrise-signed.apk",
+            "latestVersion": "1.5.112",
+            "latestVersionCode": 230,
+            "url": "https://s3-us-east-2.amazonaws.com/trustandroidapk/builds/latest_release.apk",
             "releaseNotes": [
                     "- Bug fixes"
                 ]
