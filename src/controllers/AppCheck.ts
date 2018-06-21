9e421ae650ea172f48e1071d8e26947c2b293611
Maksim <wdiabloster@gmail.com>
2018-06-22 00:16:59 +0700

    Update AppCheck.ts

--- src/controllers/AppCheck.ts
+++ src/controllers/AppCheck.ts
@@ -4,8 +4,8 @@ import { sendJSONresponse } from "../common/Utils";
 export class AppCheck {
     public android(req: Request, res: Response) {
         sendJSONresponse(res, 200, {
-            "latestVersion": "1.5.125",
-            "latestVersionCode": 247,
+            "latestVersion": "1.5.126",
+            "latestVersionCode": 248,
             "url": "https://s3-us-east-2.amazonaws.com/trustandroidapk/builds/latest_release.apk",
             "releaseNotes": [
                     "- Bug fixes"
