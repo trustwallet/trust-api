02a32dac2bc5cad8a5b2092dd281cc3b9e173ae9
Mykola <kolya182@gmail.com>
2018-06-21 16:04:00 -0700

    Add interfaces

--- src/controllers/Interfaces/ITokenPriceController.ts
+++ src/controllers/Interfaces/ITokenPriceController.ts
@@ -3,7 +3,10 @@ export interface IToken {
     symbol: string;
 }
 
-export interface IPrice {
+export interface IPrice extends ITicker, IQuotes {
+}
+
+interface ITicker {
     id: number,
     name: string,
     symbol: string,
@@ -12,17 +15,28 @@ export interface IPrice {
     circulating_supply: number | null
     total_supply: number,
     max_supply: number | null,
-    quotes: {USD: ICurrency}
     last_updated: number,
 }
 
-export interface ICurrency {
-    USD: {
+export interface IQuotes {
+    quotes: {USD: ICurrencyQuote, [key: string]: ICurrencyQuote}
+}
+
+export interface ICurrencyQuote {
         market_cap: number
         percent_change_1h: number
         percent_change_24h: number
         percent_change_7d: number
         price: number
         volume_24h: number
-    }
+}
+
+export interface IPriceDB extends ITicker, ICurrencyQuote {
+}
+
+export interface ICoefficient {
+    coefficient: number,
+    currency: string,
+    createdAt: any,
+    updatedAt: any,
 }
