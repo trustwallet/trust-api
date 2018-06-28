5e4f8e4824635d3e74e2ad7a2bdb1bd150f15541
Maksim <wdiabloster@gmail.com>
2018-06-28 14:40:10 +0700

    Update AppCheck.ts

--- src/controllers/AppCheck.ts
+++ src/controllers/AppCheck.ts
@@ -4,11 +4,15 @@ import { sendJSONresponse } from "../common/Utils";
 export class AppCheck {
     public android(req: Request, res: Response) {
         sendJSONresponse(res, 200, {
-            "latestVersion": "1.5.126",
-            "latestVersionCode": 248,
+            "latestVersion": "1.5.131",
+            "latestVersionCode": 254,
             "url": "https://s3-us-east-2.amazonaws.com/trustandroidapk/builds/latest_release.apk",
             "releaseNotes": [
-                    "- Bug fixes"
+                    "- Improved token UI - clicking on token shows transactions for that token",
+                    "- Amount validation",
+                    "- Descriptions on import fields",
+                    "- Show identicon in wallet list",
+                    "- Fixes: collectible still being show after transfer"
                 ]
             }
         )
