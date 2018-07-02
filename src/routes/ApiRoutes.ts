02420d42731f02d23525458ba1bba5e098d8e325
Mykola <kolya182@gmail.com>
2018-07-02 01:57:22 -0700

    Add route

--- src/routes/ApiRoutes.ts
+++ src/routes/ApiRoutes.ts
@@ -2,17 +2,19 @@ import  * as express from "express";
 import { DAppsController } from "../controllers/DAppsController";
 import { TokenPriceController } from "../controllers/TokenPriceController";
 import { AppCheck } from "../controllers/AppCheck";
+import { TokenInfo } from "../controllers/TokenInfo";
 
 const router = express.Router();
 
 const dAppsController = new DAppsController();
 const priceController = new TokenPriceController();
 const appCheck = new AppCheck();
+const tokenInfo = new TokenInfo();
 
 router.get("/dapps/main", dAppsController.main);
 router.get("/dapps/category/:id", dAppsController.byCategoryID);
 router.post("/tokenPrices", priceController.getTokenPrices);
-
+router.post("/tokeninfo/", tokenInfo.getTokenInfo)
 router.get("/appcheck/android", appCheck.android);
 
 export {
