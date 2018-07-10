f93b5baf576d3052b1ece033950952fdb3758f4e
Viktor Radchenko <yazexel@gmail.com>
2018-07-09 20:44:46 -0700

    Update iOS and Android checker

--- src/controllers/AppCheck.ts
+++ src/controllers/AppCheck.ts
@@ -3,9 +3,11 @@ import { sendJSONresponse } from "../common/Utils";
 
 export class AppCheck {
     public android(req: Request, res: Response) {
+        const version = "1.5.131"
+        const build = 254
         sendJSONresponse(res, 200, {
-            "latestVersion": "1.5.131",
-            "latestVersionCode": 254,
+            "latestVersion": version,
+            "latestVersionCode": build,
             "url": "https://s3-us-east-2.amazonaws.com/trustandroidapk/builds/latest_release.apk",
             "releaseNotes": [
                     "- Improved token UI - clicking on token shows transactions for that token",
@@ -13,7 +15,10 @@ export class AppCheck {
                     "- Descriptions on import fields",
                     "- Show identicon in wallet list",
                     "- Fixes: collectible still being show after transfer"
-                ]
+            ],
+            "title": "Update Available",
+            "description": "A new version of Trust Wallet is available. Please update to version " + version,
+            "force": false
             }
         )
     }
@@ -26,7 +31,7 @@ export class AppCheck {
             "latestVersionCode": build,
             "url": "https://itunes.apple.com/us/app/trust-ethereum-wallet/id1288339409?mt=8",
             "title": "Update Available",
-            "description": "A new version of Trust Wallet is available. Please Update to version " + version,
+            "description": "A new version of Trust Wallet is available. Please update to version " + version,
             "force": false
             },
         )
