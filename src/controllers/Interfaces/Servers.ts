a226ca5aa4d9814742e0b4f4b4c25c7bb0eedccc
Mykola <kolya182@gmail.com>
2018-07-20 14:07:29 -0700

    Redirect ethereum transactions

---
+++ src/controllers/Interfaces/Servers.ts
@@ -0,0 +1,12 @@
+export enum Nodes {
+    Ethereum = "https://api.trustwalletapp.com/",
+    Localhost = "http://localhost:8000/"
+}
+
+export enum Routes {
+    Transactions = "transactions"
+}
+
+export enum Networks {
+    ethereum = "ethereum"
+}
--- src/routes/ApiRoutes.ts
+++ src/routes/ApiRoutes.ts
@@ -3,8 +3,9 @@ import { DAppsController } from "../controllers/DAppsController";
 import { TokenPriceController } from "../controllers/TokenPriceController";
 import { AppCheck } from "../controllers/AppCheck";
 import { TokenInfo } from "../controllers/TokenInfo";
-
+import { Nodes, Routes, Networks } from "../controllers/Interfaces/Servers"
 const router = express.Router();
+import url = require("url");
 
 const dAppsController = new DAppsController();
 const priceController = new TokenPriceController();
@@ -17,6 +18,15 @@ router.post("/tokenPrices", priceController.getTokenPrices);
 router.get("/appcheck/android", appCheck.android);
 // Token info
 router.get("/tokeninfo/:networkid/:address?", tokenInfo.getTokenInfo);
+
+// Ethereum routes
+router.get(`/ethereum/transactions`, (req, res) => {
+    const redirectUrl = url.format({
+        pathname: `${Nodes.Localhost}${Routes.Transactions}`,
+        query: req.query
+    })
+    res.redirect(redirectUrl)
+})
 
 export {
     router
