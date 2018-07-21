5f78b3d8dfcb37e9f4fcbbd4f623202b5f4aca96
Mykola <kolya182@gmail.com>
2018-07-21 15:21:10 -0700

    Rename enums

--- src/controllers/Interfaces/Servers.ts
+++ src/controllers/Interfaces/Servers.ts
@@ -1,14 +1,14 @@
 export enum Nodes {
     ethereum = "https://api.trustwalletapp.com/",
-    ethereumClassic = "https://trust-classic.herokuapp.com/",
     poa = "https://trust-poa.herokuapp.com/",
-    callisto = "https://trust-callisto.herokuapp.com/",
-    localhost = "http://localhost:8000/"
+    // classic = "https://trust-classic.herokuapp.com/",
+    // callisto = "https://trust-callisto.herokuapp.com/",
+    // localhost = "http://localhost:8000/"
 }
 
 // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
 
-export enum CoinTypes {
+export enum CoinTypeIndex {
     ethereum = 60,
     // classic = 61,
     // poa = 178,
