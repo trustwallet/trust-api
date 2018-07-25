8420e603f955fb22589427f0e1b99bca17004f93
Mykola <kolya182@gmail.com>
2018-07-24 20:05:39 -0700

    Enable all supported networks

--- src/controllers/Interfaces/Servers.ts
+++ src/controllers/Interfaces/Servers.ts
@@ -1,23 +1,26 @@
 export enum Nodes {
     ethereum = "https://api.trustwalletapp.com/",
+    classic = "https://trust-classic.herokuapp.com/",
     poa = "https://trust-poa.herokuapp.com/",
-    // classic = "https://trust-classic.herokuapp.com/",
-    // callisto = "https://trust-callisto.herokuapp.com/",
-    // localhost = "http://localhost:8000/"
+    callisto = "https://trust-callisto.herokuapp.com/",
+    localhost = "http://localhost:8000/"
 }
 
 export enum Endpoints {
     TokensList = "tokens/list",
     Assets = "assets",
     Transactions = "transactions",
-    TransactionId = "transactions/"
+    TransactionId = "transactions/",
+    RegisterDevice = "push/register",
+    UnegisterDevice = "push/unregister"
 }
 // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
 
 export enum CoinTypeIndex {
     ethereum = 60,
-    // classic = 61,
+    classic = 61,
     poa = 178,
-    // callisto = 820,
-    // goChain = 6060
+    callisto = 820,
+    // gochain = 6060,
+    // localhost = 8000 // Uncomment for test pourpouse
 }
