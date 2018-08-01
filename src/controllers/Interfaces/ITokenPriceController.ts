7f709fcb90e789481e586c8e3b244a5169564e1b
Mykola <kolya182@gmail.com>
2018-08-01 13:13:43 -0700

    Update interfaces

--- src/controllers/Interfaces/ITokenPriceController.ts
+++ src/controllers/Interfaces/ITokenPriceController.ts
@@ -3,20 +3,40 @@ export interface IToken {
     symbol: string;
 }
 
-export interface IPrice {
-    id: string,
+export interface IPrice extends ITicker, IQuotes {
+}
+
+interface ITicker {
+    id: number,
     name: string,
     symbol: string,
-    rank: string,
-    price_usd: string,
-    price_btc: string,
-    "24h_volume_usd": string,
-    market_cap_usd: string,
-    available_supply: string,
-    total_supply: string,
-    max_supply: null,
-    percent_change_1h: string,
-    percent_change_24h: string,
-    percent_change_7d: string,
-    last_updated: string
+    website_slug: string
+    rank: number,
+    circulating_supply: number | null
+    total_supply: number,
+    max_supply: number | null,
+    last_updated: number,
+}
+
+export interface IQuotes {
+    quotes: {USD: ICurrencyQuote, [key: string]: ICurrencyQuote}
+}
+
+export interface ICurrencyQuote {
+        market_cap: number
+        percent_change_1h: number
+        percent_change_24h: number
+        percent_change_7d: number
+        price: number
+        volume_24h: number
+}
+
+export interface IPriceDB extends ITicker, ICurrencyQuote {
+}
+
+export interface ICoefficient {
+    coefficient: number,
+    currency: string,
+    createdAt: any,
+    updatedAt: any,
 }
