755945af79754d413330f161624f7a899b679b3b
Maksim Rasputin <133312+madcake@users.noreply.github.com>
2018-09-27 05:32:50 +0700

    Bump android version

--- src/controllers/AppCheck.ts
+++ src/controllers/AppCheck.ts
@@ -3,8 +3,8 @@ import { sendJSONresponse } from "../common/Utils";
 
 export class AppCheck {
     public android(req: Request, res: Response) {
-        const version = "1.6.137"
-        const build = 313
+        const version = "1.6.140"
+        const build = 315
         sendJSONresponse(res, 200, {
             "latestVersion": version,
             "latestVersionCode": build,
